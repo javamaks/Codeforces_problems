e523cdd0b1d5b9ec00cdeca5434cc2865cd200c9
Жава <63841611+javamaks@users.noreply.github.com>
2024-09-01 23:46:41 +0500

    282A_Bit++.cpp

--- 282A_Bit++.cpp
+++ 282A_Bit++.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int x = 0;
+    int n;
+    cin >> n;
+    
+    string num;
+    for(int i = 0; i < n; i++){
+        cin >> num;
+        
+        if(num.find("--") != string::npos){
+            x--;
+        }
+        if(num.find("++") != string::npos){
+            x++;
+        }
+    }
+    cout << x << endl;
+    return 0;
+}
 
